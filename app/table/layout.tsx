2c01905cb33f51f93056ccf67a9cc3f2d25bd079
nely1 <nely@student.unimelb.edu.au>
2024-01-12 18:02:57 +1100

    made tables and styled hovering

--- app/table/layout.tsx
+++ app/table/layout.tsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import './style.css'
 
-const layout = () => {
+const layout = ({children}) => {
   return (
-    <nav className="navbar">
-    <a href="#" className="nav-logo">NES</a>
-     <ul className="nav-menu">
+    <html lang="en">
+    <body>
+      <nav className="navbar">
+      <a href="#" className="nav-logo">NES</a>
+      <ul className="nav-menu">
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
         <li><a href="#">Login</a></li>
-     </ul>
-    </nav>
+      </ul>
+      </nav>
+      
+      {children}
+      </body>
+    </html>
   )
 }
 
--- app/table/page.tsx
+++ app/table/page.tsx
@@ -11,10 +11,22 @@ const tablepage = () => {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>1</td>
-        <td>Software engineer position for NES web platform</td>
-        <td>101</td>
+      <tr className="hover:bg-gray-100">
+        <td><button  className="w-full text-left">1</button></td>
+        <td><button  className="w-full text-left">Software engineer position for NES web platform</button></td>
+        <td><button className="w-full text-left">101</button></td>
+      </tr>
+
+      <tr className="hover:bg-gray-100">
+        <td><button  className="w-full text-left">2</button></td>
+        <td><button  className="w-full text-left">Project Manager position for NES web platform</button></td>
+        <td><button className="w-full text-left">200</button></td>
+      </tr>
+      
+      <tr className="hover:bg-gray-100">
+        <td><button  className="w-full text-left">3</button></td>
+        <td><button  className="w-full text-left">Intern position for NES web platform</button></td>
+        <td><button className="w-full text-left">10000</button></td>
       </tr>
     </tbody>
   </table>
