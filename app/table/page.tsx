203ed780b1d96180702795bc5970e5fb1021b60e
Mirtekore <engjiaee@gmail.com>
2024-01-12 16:26:07 +1100

    Created basic table layout

---
+++ app/table/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const tablepage = () => {
+  return (
+    <table>
+    <thead>
+      <tr>
+        <th>No.</th>
+        <th>Job posting</th>
+        <th>Number of applications</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Software engineer position for NES web platform</td>
+        <td>101</td>
+      </tr>
+    </tbody>
+  </table>
+  )
+}
+
+export default tablepage
