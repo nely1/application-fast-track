1c647b403337c213a342b52f6df726fe1ec4c33e
nely1 <nely@student.unimelb.edu.au>
2024-01-15 17:04:11 +1100

    testing sign in and out

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ import { useRouter, redirect } from 'next/navigation';
 import { getUserSession } from '@/lib/session';
 import { GET } from './api/auth/[...nextauth]/route';
 // this is the login page
-export default async function Home() {
+export default function Home() {
 
-  const user = await getUserSession();
-  console.log(JSON.stringify(user));
+
+  
   // if (user) {
   //   redirect('/table');
   // }
@@ -22,10 +22,13 @@ export default async function Home() {
  
   }
 
+  const handleSignOut = () => {
+    signOut();
+  }
+
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      {JSON.stringify(user)}
       <Image
               src="/logo.svg"
               alt="NES Logo"
@@ -35,7 +38,7 @@ export default async function Home() {
               priority
             />
       <button type="button" onClick={handleClick} className="rounded-lg px-10 py-3 hover:bg-green-800 bg-green-700 mt-20">Sign in with Google</button> 
-      
+      <button type="button" onClick={handleSignOut} className="rounded-lg px-10 py-3 hover:bg-green-800 bg-green-700 mt-20">Sign out</button> 
 
     </main>
   )
