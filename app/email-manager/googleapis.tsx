3da4f36752114500ba9ab2260852632f9455b57d
Mirtekore <engjiaee@gmail.com>
2024-01-18 15:03:59 +1100

    Added retrieval of email subject

--- app/email-manager/googleapis.tsx
+++ app/email-manager/googleapis.tsx
@@ -3,6 +3,7 @@ const path = require('path');
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
+// const Base64 = require('js-base64');
 
 // If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
@@ -11,6 +12,13 @@ const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
 // time.
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
+
+
+function decodeBase64(str) {
+  str = str.replace(/_/g, '/').replace(/-/g, '+');
+  return Buffer.from(str, "base64");
+}
+
 
 /**
  * Reads previously authorized credentials from the save file.
@@ -70,20 +78,46 @@ async function authorize() {
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-async function listLabels(auth) {
+async function getEmails(auth) {
+  const emailNumber = 0;
   const gmail = google.gmail({version: 'v1', auth});
-  const res = await gmail.users.labels.list({
+  const res = await gmail.users.messages.list({
     userId: 'me',
   });
-  const labels = res.data.labels;
-  if (!labels || labels.length === 0) {
-    console.log('No labels found.');
+  
+  const messages = res.data;
+  if (!messages || messages.length === 0) {
+    console.log('No emails found.');
     return;
+  } else {
+
+    const firstEmail = await gmail.users.messages.get({
+      userId: 'me',
+      id: messages.messages[emailNumber].id,
+    });
+
+    console.log("This is the subject title: ")
+
+    for (const header of firstEmail.data.payload.headers) {
+        if (header.name === 'Subject') {
+            console.log(header.value);
+        }
+    }
+
+    // console.log("\nThis is the attachments: ")
+    // const attachmentId = 'ANGjdJ-2xwZRda_PkAEoYM5big0DJNfZODBhCdqffhw66Xe-nu2ZosvrPMAXQs1NkpDIskrxQtjxZJmmrmzmKl_j9aT2ThIe1sbKxhTe9of1IliS8biKSr3l4moXE_UezK6Qv-RKIWlqUsQaBHbbbgHgC8gbd38Z6pmYyyoICFlqWi2XYvqb4vkpMwAlJo-chjBmIcd-yG9FVh1V04de-Gm-hUgfGXCUKo5lvI_Z0l3zAhzfSbMgrROgllffzvQH6ipct2_mQw8qLJL-WCAUSo8FQYM0oKAxmDAL9cV2YgCajkhsEANVSzPlPaO4TWk'
+
+    // const resume = await google.gmail('v1').users.messages.attachments.get({
+    //   userId: 'me',
+    //   id: attachmentId
+    // });
+    // console.log(resume)
+    // const attachmentPart = firstEmail.data.payload.parts[1];
+    // fs.writeFile(attachmentPart.filename, decodeBase64(attachmentPart.data));
+
   }
-  console.log('Labels:');
-  labels.forEach((label) => {
-    console.log(`- ${label.name}`);
-  });
+
+
 }
 
-authorize().then(listLabels).catch(console.error);
+authorize().then(getEmails).catch(console.error);
